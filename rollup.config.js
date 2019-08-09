405b448a7be6992b4681495da036f6bef08da083
Luciano Franca <lucianopf@outlook.com>
2019-08-08 21:36:27 -0300

    wip

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,12 @@ import svelte from 'rollup-plugin-svelte';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
+import replace from 'rollup-plugin-replace';
 import { terser } from 'rollup-plugin-terser';
 
 const production = !process.env.ROLLUP_WATCH;
+
+const appVersion = process.env.CIRCLE_TAG || "'dev'";
 
 export default {
 	input: 'src/main.js',
@@ -15,6 +18,9 @@ export default {
 		file: 'public/bundle.js'
 	},
 	plugins: [
+    replace({
+      'VERSION': appVersion,
+    }),
 		svelte({
 			dev: !production,
 			css: css => {
